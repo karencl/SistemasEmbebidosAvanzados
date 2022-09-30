9971fbac7a6112ea7b645573dc02acfb0b5ff144
Andrix777 <114591012+Andrix777@users.noreply.github.com>
2022-09-29 23:52:56 -0500

    Update exercise03.py

--- Intro-py-exercises/exercise03.py
+++ Intro-py-exercises/exercise03.py
@@ -1,7 +1,7 @@
 ## =================================================================
 ##
 ## File: exercise03.py
-## Author(s):
+## Author(s): Andrea Gonzalez Arredondo A01351820 Karen Cebreros Lopez A01704254
 ## Description: This file contains the code that implements the
 ##				enumeration sort algorithm. The time this implementation
 ##				takes ill be used as the basis to calculate the
@@ -18,25 +18,29 @@ import time
 
 SIZE = 10000 ##1e5
 
-## Place your code here
-
 if __name__ == "__main__":
-    array = [0] * SIZE
+    array1 = [0] * SIZE
+    array2 = [0] * SIZE
 
-    utils.randomArray(array)
-    utils.displayArray("before", array)
+    utils.randomArray(array1)
+    utils.displayArray("before", array1)
 
     print("Starting...")
-    startTime = endTime = ms = 0
-	result = 0
+    startTime = endTime = ms = result = 0
+
     for i in range(utils.N):
         startTime = time.time() * 1000
 
-        ## Place your code here
+        for j in range(SIZE):
+            max = SIZE - 1
+            for k in range(SIZE):
+                if ((array1[j] < array1[k]) or (array1[j] == array1[k] and j < k)):
+                    max -= 1
+            array2[max] = array1[j];
 
         endTime = time.time() * 1000
 
         ms = ms + (endTime - startTime)
 
-    utils.displayArray("after", array)
+    utils.displayArray("after", array2)
     print("avg time = ", (ms / utils.N), " ms")
